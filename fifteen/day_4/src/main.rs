e04be9852facd93c49cd3d256c0f4a5e5884196b
Juan Rodriguez <jrodri89@uwo.ca>
2020-05-02 11:13:25 -0400

    partway to processing strings into bit vectors

--- fifteen/day_4/src/main.rs
+++ fifteen/day_4/src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    const KEY : &str = "iwrupvqb";
+    const _KEY : &str = "iwrupvqb";
+    const _H : (u32, u32, u32, u32) = (0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476);
+
+    //assert_eq!([0],_md4("hello"));
+    let vec = Vec::new(0; 5);
+    assert_eq!(vec, [0, 0, 0, 0, 0]);
+}
+
+fn _md4(input : &str) -> Vec<u32> {
+    // returns hex bytecode value of encrypted string
+    //
+    // left rotation: bits that fall off at left added at right
+    
+    let bytes = input.to_string().into_bytes();
+    let mut bits : Vec<u32> = Vec::new();
+
+    for entry in bytes {
+        let bin : String =  format!("{:b}", entry);
+        for bit in bin.chars() {
+            bits.push(bit.to_digit(10).expect("could not read bit"));
+        }
+    }
+    bits
 }
