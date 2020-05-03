919372e7a5ecc02aa78dcb318b24068816cd0e7c
Juan Rodriguez <jrodri89@uwo.ca>
2020-05-03 09:32:28 -0400

    preprocessing complete

--- fifteen/day_4/src/main.rs
+++ fifteen/day_4/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     test();
 }
 
-fn into_bits(input : &str) -> Vec<u32> {
+fn into_le_bits(input : &str) -> Vec<u32> {
     // returns vector containing binary representation of input string
     //
     // left rotation: bits that fall off at left added at right
@@ -48,7 +48,7 @@ fn append_bits(bit_stream : Vec<u32>) -> Vec<u32> {
     
 }
 
-fn to_64_bit(input : &str) { 
+fn to_64_bit(input : &str) -> Vec<u32> { 
     // returns 64-bit version of bit stream (mod 2^64)
 
     let max : u128 = 2_u128.pow(64);
@@ -64,19 +64,52 @@ fn to_64_bit(input : &str) {
     let bin_num : u128 = u128::from_str_radix(&bin_string, 2).unwrap();
     let bin_num : u128 = bin_num % max;
 
-    let bytes : [u8;16] = bin_num.to_le_bytes();
+    let bytes : [u8;16] = bin_num.to_be_bytes();
+    let mut bits : Vec<u32> = Vec::new();
    
+    for entry in bytes.iter() {
+        let bin_string : String = format!("{:b}", entry);
+        for n in bin_string.chars() {
+            bits.push(n.to_digit(10).expect("could not read bit"));
+        }
+    }
+    bits.reverse();
+
+    while bits.len() < 64 {
+        bits.push(0);
+    }
+
+    bits
+}
+
+fn preprocess(input : &str) -> Vec<u32> {
+    // returns a vector containing preprocessed string (in binary)
+    // output vector length will always be a multiple of 512
+    let mut input_le = into_le_bits(input);
+    let mut input_64 = to_64_bit(input);
+
+    input_le.append(&mut input_64);
+
+    let processed = input_le;
+    
+    processed
 }
 
 fn test() {
     // bit stream of a = 97 in binary (ASCII representation of "a")
-    assert_eq!(into_bits("a"), [1, 0, 0, 0 , 0, 1, 1]); 
+    let mut a_le : Vec<u32> = [1, 0, 0, 0, 0, 1, 1].to_vec(); // a in little-endian (le) form
+    assert_eq!(into_le_bits("a"), a_le); 
     
     assert_eq!(448, append_bits(vec![1;449]).len() % 512);
     assert_eq!(448, append_bits(vec![1;447]).len() % 512);
     assert_eq!(448, append_bits(vec![1;448]).len() % 512);
     assert_eq!(448, append_bits(vec![1;969]).len() % 512);
 
-    assert_eq!(448, append_bits(into_bits("a")).len() % 512);
+    assert_eq!(448, append_bits(into_le_bits("a")).len() % 512);
+
+    a_le.append(&mut vec![0; 57]);
+    let a_le_64 : Vec<u32> = a_le;
+
+    assert_eq!(a_le_64, to_64_bit("a"));
 
 }
